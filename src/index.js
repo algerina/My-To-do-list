66a2f39c1400c05754bf97c1da12d0a9a5949613
Amel Khiri <53347966+algerina@users.noreply.github.com>
2021-11-05 18:45:59 +0100

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import "./style.css";
+import './style.css';
 
 // const updateLocalStorage = require('./storage.js');
 
-const renderTasks = require("./render.js");
+const renderTasks = require('./render.js');
 
-const storedTasks = JSON.parse(localStorage.getItem("tasks"));
+const storedTasks = JSON.parse(localStorage.getItem('tasks'));
 const tasks = storedTasks || [];
 
 // const list = document.getElementById('todoList');
-const addInput = document.getElementById("newTask");
-const addButton = document.getElementById("addBtn");
+const addInput = document.getElementById('newTask');
+const addButton = document.getElementById('addBtn');
 class Todo {
   constructor(description, completed = false) {
     this.description = description;
@@ -21,7 +21,7 @@ class Todo {
     tasks.push({
       description: this.description,
       completed: this.completed,
-      index: this.index
+      index: this.index,
     });
   }
 }
@@ -33,22 +33,22 @@ if (storedTasks && storedTasks.length) {
 function createTask() {
   const text = addInput.value;
 
-  if (text === "") {
+  if (text === '') {
     return;
   }
   const task = new Todo(text);
 
   tasks.push(task);
-  localStorage.setItem("tasks", JSON.stringify(tasks));
+  localStorage.setItem('tasks', JSON.stringify(tasks));
   renderTasks.renderTasks(tasks);
 
-  addInput.value = "";
+  addInput.value = '';
 }
 
-addButton.addEventListener("click", createTask);
+addButton.addEventListener('click', createTask);
 
-addInput.addEventListener("keypress", event => {
-  if (event.key === "Enter") {
+addInput.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
     createTask();
   }
 });
